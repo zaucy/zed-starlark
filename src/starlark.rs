b1468756ae7636418c3e04c187d9f7844c35406c
Rashid Almheiri <r.muhairi@pm.me>
2024-07-22 19:55:26 +0400

    feat: Include support for buck2 (#4)

--- src/starlark.rs
+++ src/starlark.rs
@@ -1,102 +1,43 @@
-use std::fs;
+use starpls::Starpls;
 use zed_extension_api::{self as zed, Result};
 
+mod starpls;
+
 struct StarlarkExtension {
-    cached_binary_path: Option<String>,
-}
-
-impl StarlarkExtension {
-    fn language_server_binary_path(
-        &mut self,
-        language_server_id: &zed::LanguageServerId,
-    ) -> Result<String> {
-        if let Some(path) = &self.cached_binary_path {
-            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
-                return Ok(path.clone());
-            }
-        }
-
-        zed::set_language_server_installation_status(
-            language_server_id,
-            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
-        );
-        let (platform, arch) = zed::current_platform();
-        let release = zed::latest_github_release(
-            "withered-magic/starpls",
-            zed::GithubReleaseOptions {
-                require_assets: true,
-                pre_release: false,
-            },
-        )?;
-
-        let exe_suffix = match platform {
-            zed::Os::Windows => ".exe",
-            _ => "",
-        };
-        let asset_name = format!(
-            "starpls-{os}-{arch}{exe_suffix}",
-            arch = match arch {
-                zed::Architecture::Aarch64 => "arm64",
-                zed::Architecture::X86 => "x86", // not supported
-                zed::Architecture::X8664 => "amd64",
-            },
-            os = match platform {
-                zed::Os::Mac => "darwin",
-                zed::Os::Linux => "linux",
-                zed::Os::Windows => "windows",
-            },
-        );
-
-        let asset = release
-            .assets
-            .iter()
-            .find(|asset| asset.name == asset_name)
-            .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
-
-        let version_dir = format!("starpls-{}", release.version);
-        let binary_path = format!("{version_dir}/starpls{exe_suffix}");
-
-        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
-            zed::set_language_server_installation_status(
-                language_server_id,
-                &zed::LanguageServerInstallationStatus::Downloading,
-            );
-
-            _ = fs::create_dir(version_dir);
-
-            zed::download_file(
-                &asset.download_url,
-                &binary_path,
-                zed::DownloadedFileType::Uncompressed,
-            )
-            .map_err(|e| format!("failed to download file: {e}"))?;
-
-            zed::make_file_executable(&binary_path)
-                .map_err(|e| format!("failed to make file executable: {e}"))?;
-        }
-
-        self.cached_binary_path = Some(binary_path.clone());
-        Ok(binary_path)
-    }
+    starpls: Option<Starpls>,
 }
 
 impl zed::Extension for StarlarkExtension {
     fn new() -> Self {
-        Self {
-            cached_binary_path: None,
-        }
+        Self { starpls: None }
     }
 
     fn language_server_command(
         &mut self,
         language_server_id: &zed::LanguageServerId,
-        _worktree: &zed::Worktree,
+        worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        Ok(zed::Command {
-            command: self.language_server_binary_path(language_server_id)?,
-            args: vec![],
-            env: Default::default(),
-        })
+        match language_server_id.as_ref() {
+            "starpls" => {
+                let starpls = self.starpls.get_or_insert_with(|| Starpls::new());
+                Ok(zed::Command {
+                    command: starpls.language_server_binary_path(language_server_id)?,
+                    args: vec![],
+                    env: Default::default(),
+                })
+            }
+            "buck2-lsp" => {
+                let path = worktree.which("buck2").ok_or_else(|| {
+                    "buck2 must be installed. The LSP is bundled with the buck2 cli.".to_string()
+                })?;
+                Ok(zed::Command {
+                    command: path,
+                    args: vec!["lsp".to_string()],
+                    env: Default::default(),
+                })
+            }
+            language_server_id => Err(format!("unknown language server: {language_server_id}")),
+        }
     }
 }
 
